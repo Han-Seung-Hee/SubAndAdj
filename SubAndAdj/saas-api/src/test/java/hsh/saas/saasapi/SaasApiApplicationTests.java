ea75c5ec09fb506cf60f461be1bbc0a5917772e7
Han-Seung-Hee <xorhd1222@gmail.com>
2026-02-27 15:51:22 +0900

    initialize project structure with Gradle configuration and basic application setup

---
+++ SubAndAdj/saas-api/src/test/java/hsh/saas/saasapi/SaasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hsh.saas.saasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ SubAndAdj/src/main/java/hsh/saas/subandadj/SubAndAdjApplication.java
@@ -0,0 +1,13 @@
+package hsh.saas.subandadj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubAndAdjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubAndAdjApplication.class, args);
+    }
+
+}
