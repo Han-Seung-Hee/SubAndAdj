ee8b78bdc94286f99dd9930fbc53500525a1d565
Han-Seung-Hee <xorhd1222@gmail.com>
2026-04-20 20:31:04 +0900

    스캔 범위 변경

--- SubAndAdj/saas-api/src/main/java/hsh/saas/saasapi/SaasApiApplication.java
+++ SubAndAdj/saas-api/src/main/java/hsh/saas/saasapi/SaasApiApplication.java
@@ -6,12 +6,11 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "hsh.saas") // [핵심] hsh.saas 하위의 모든 패키지를 스캔합니다.
-@EntityScan(basePackages = "hsh.saas.saascore.domain") // 엔티티 스캔 경로 설정
-@EnableJpaRepositories(basePackages = "hsh.saas.saascore.repository") // JPA 리포지토리 스캔 경로 설정
+@EntityScan(basePackages = "hsh.saas.saascore") // 엔티티 스캔 경로 설정
+@EnableJpaRepositories(basePackages = "hsh.saas.saascore") // JPA 리포지토리 스캔 경로 설정
 public class SaasApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SaasApiApplication.class, args);
     }
-
 }
